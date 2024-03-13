66182fccbfc89f1a2d1def8b8173e11541f168a3
Jeevan <31155545+jeevan0920@users.noreply.github.com>
2024-03-13 21:56:15 +0530

    Rewriting back to string after the correcting json body

--- schema-alias-map-builder/src/main.rs
+++ schema-alias-map-builder/src/main.rs
@@ -122,7 +122,7 @@ fn alias_decode(mut json_body: &mut Value, alias_map: &HashMap<String, String>) 
             // call alias_decode for each json object
             alias_decode(&mut co_json, alias_map);
             // insert the corrected co_json string into the json_body
-            json_body["co"] = serde_json::to_value(co_json).unwrap();
+            json_body["co"] = serde_json::Value::String(serde_json::to_string(&co_json).unwrap());
         }
     }
 
@@ -135,28 +135,10 @@ fn alias_decode(mut json_body: &mut Value, alias_map: &HashMap<String, String>) 
             // call alias_decode for the json object
             alias_decode(&mut ue_pr_json, alias_map);
             // insert the corrected ue_pr_json string into the json_body
-            json_body["ue_pr"] = serde_json::to_value(ue_pr_json).unwrap();
+            json_body["ue_pr"] =
+                serde_json::Value::String(serde_json::to_string(&ue_pr_json).unwrap());
         }
     }
-
-    // let contexts_jn: Value = serde_json::from_str(&json_body["data"]["co"].to_string()).unwrap();
-    // println!("======================= contexts json ======================");
-    // println!("contexts_jn: {:#?}", json_body["data"][0]["co"]);
-    // println!("===========================================================");
-    // let json_str = json_body["data"][0]["co"].clone();
-    // let mut contexts_jn: Value = serde_json::from_str(json_str.as_str().unwrap()).unwrap();
-    // println!("contexts_jn json : {:#?}", contexts_jn);
-    // println!("===========================================================");
-
-    // let json_str = json_body["data"][0]["co"].clone();
-    // let mut contexts_jn: Value = serde_json::from_str(json_str.as_str().unwrap()).unwrap();
-
-    // // recursively correct the aliased data value's contextsjson
-    // if let Some(data_array) = contexts_jn["data"].as_array_mut() {
-    //     for obj in data_array {
-    //         alias_decode(obj, alias_map);
-    //     }
-    // }
 
     println!("after alias_decode: {:#?}", json_body);
 }
